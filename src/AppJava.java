783b01a2e1562b33089d74a34576181dc38198c7
unknown <tomnoii.da@gmail.com>
2017-04-10 12:32:05 +0700

    JJ

---
+++ src/AppJava.java
@@ -0,0 +1,18 @@
+
+public class AppJava {
+	void aaa (int x){
+		int a=2;
+		a=a+x;
+		System.out.println(a);
+		
+	}
+
+	public static void main(String[] args) {
+		AppJava x=new AppJava();
+		x.aaa(8);
+		
+		// TODO Auto-generated method stub
+
+	}
+
+}
